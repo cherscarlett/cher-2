896f301289fa2a56d71dce8991aecb727d135ba7
Cher Scarlett <cher@controlzee.com>
2023-02-20 12:18:11 -0800

    Udate

--- src/api/content.js
+++ src/api/content.js
@@ -1,21 +1,21 @@
 export default {
-    home: {
-        heading: 'Howdy.',
-        subHeading: 'My Coding Playlist',
-        paragraphs:  [
-            'I am a software engineer with a non-traditional background who advocates for fair labor practices, justice, and equity in tech.',
-            'I\'ve been programming for more than 20 years and have worked in a variety of industries in large corporations such as Apple, Activision Blizzard, Starbucks, and USA Today. I specialize in front-end programming, but am effectively a full-stack engineer and have professionally touched most every part of the software development pipeline.',
-            'I am most interested in space, esports, video games, music, film, and design systems.'
-        ]
-    },
-    resume: {
-        heading: 'Work History',
-    },
-    fun: {
-        heading: 'Fun Stuff',
-        paragraphs:  [
-            'In order to make myself stand out as a candidate early in my career, and to fight for more equitable pay, I built fun themed resume applications and other things.',
-            'Here are a few of my favorites, along with other things I\'ve built in my free time.'
-        ]
-    }
-}
+  home: {
+    heading: "Howdy.",
+    subHeading: "My Coding Playlist",
+    paragraphs: [
+      'I am a software engineer and memoirist. I am the author of "Forbidden Fruit" and a to-be-announced book. I founded #AppleToo and advocate for fair labor practices, justice, and equity in tech.',
+      "I've been programming for more than 20 years and have worked in a variety of industries in large corporations such as Apple, Activision Blizzard, Starbucks, and USA Today. I specialize in front-end programming, but am effectively a full-stack engineer and have professionally touched most every part of the software development pipeline.",
+      "I am most interested in space, esports, video games, music, film, and design systems.",
+    ],
+  },
+  resume: {
+    heading: "Work History",
+  },
+  fun: {
+    heading: "Fun Stuff",
+    paragraphs: [
+      "In order to make myself stand out as a candidate early in my career, and to fight for more equitable pay, I built fun themed resume applications and other things.",
+      "Here are a few of my favorites, along with other things I've built in my free time.",
+    ],
+  },
+};
